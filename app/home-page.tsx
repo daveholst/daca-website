26e21cc5bcb47caebd9198dde53d6c1273b7c478
Dave Holst <daveholst@gmail.com>
2023-07-30 09:12:49 +0800

    local dev working

--- app/home-page.tsx
+++ app/home-page.tsx
@@ -1,24 +1,24 @@
-"use client";
+'use client'
 
-import RoundPlay from "@/components/icons/RoundPlay";
-import { useState } from "react";
-import ReactPlayer from "react-player/youtube";
+import RoundPlay from '@/components/icons/RoundPlay'
+import { useState } from 'react'
+import ReactPlayer from 'react-player/youtube'
 
 // This is a Client Component. It receives data as props and
 // has access to state and effects just like Page components
 // in the `pages` directory.
-export default function HomePage({ test }) {
-  const [playVideo, setPlayVideo] = useState(false);
-  console.log(test);
+export default function HomePage() {
+  const [playVideo, setPlayVideo] = useState(false)
+  // console.log(test)
   return (
     <>
       {/* Video Intro & CTA */}
       <div className="h-screen bg-cover bg-center bg-[url('/img/fire-bikes-ute.jpeg')] ">
         <div className="h-screen flex items-center justify-center flex-col bg-black/50">
           <h2 className="text-white font-sans2 text-5xl font-bold ">
-            RAW ADVENTURE {test}
+            RAW ADVENTURE
           </h2>
-          <p className="font-sans3 text-orange-100 mt-3 text-3xl font-extralight text-center lg:w-1/3">
+          <p className=" font-sans3 text-orange-100 mt-3 text-3xl font-extralight text-center lg:w-1/3">
             AUSTRALIA’S MOST REMOTE EPIC OFF-ROAD TOURS.
           </p>
           {!playVideo && (
@@ -33,8 +33,8 @@ export default function HomePage({ test }) {
             //TODO Maybe just take this out and host it on a CDN so I can get full control and kill the branding
             <div className="xl:container aspect-video w-full">
               <ReactPlayer
-                width={"100%"}
-                height={"100%"}
+                width={'100%'}
+                height={'100%'}
                 // playing
                 url="https://www.youtube.com/watch?v=38LLPkpbOXs"
                 config={{
@@ -46,5 +46,5 @@ export default function HomePage({ test }) {
         </div>
       </div>
     </>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,26 @@
 // Import your Client Component
-import HomePage from './home-page';
+import HomePage from './home-page'
 
 // async function getPosts() {
 //   const res = await fetch('https://...');
 //   const posts = await res.json();
 //   return posts;
 // }
-function delay(ms: number): Promise<void> {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
+// function delay(ms: number): Promise<void> {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, ms);
+//   });
+// }
 
-async function asyncTimeout() {
-  console.log('Start');
-  await delay(100);
-  console.log('End');
-  return 'end';
-}
+// async function asyncTimeout() {
+//   console.log('Start');
+//   await delay(100);
+//   console.log('End');
+//   return 'end';
+// }
 
 export default async function Page() {
   // Fetch data directly in a Server Component
-  const returnString = await asyncTimeout();
-  console.log(returnString);
   // Forward fetched data to your Client Component
-  return <HomePage test={returnString} />;
+  return <HomePage />
 }
