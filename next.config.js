5e37ab4092b3745333dd9d7d586f8a80132b97eb
Dave Holst <daveholst@gmail.com>
2023-09-07 21:11:45 +0800

    move to pnpm, localdynamo tweaks, next config cleanup

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  output: 'standalone',
-  // assetPrefix: isProd ? 'https://d3nq3ed4qzq05o.cloudfront.net/' : undefined,
-  images: {
-    loader: 'custom',
-    // Custom loader that strips out all query params that worn't working through cloudfront
-    loaderFile: './src/loaders/cloudfront.js',
-  },
   experimental: {
     serverActions: true,
   },
