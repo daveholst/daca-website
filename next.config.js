93dd1bfe091df5f5f206e719523f5d99681286bf
Dave Holst <daveholst@gmail.com>
2023-12-27 12:48:24 +0800

    bump next and SST to latest

--- next.config.js
+++ next.config.js
@@ -3,14 +3,11 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   webpack: (config, { webpack, isServer, nextRuntime }) => {
     // Avoid AWS SDK Node.js require issue
     if (isServer && nextRuntime === 'nodejs')
       config.plugins.push(
-        new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ })
+        new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ }),
       )
     return config
   },
